86c1884af122e76c0166dfa0d59c069d66d474de
Amila Abeygunasekara <auabey@gmail.com>
2026-02-21 17:53:49 +0000

    Refactor image dumping script, also output gs_ply

--- image_renderer/dump_images.py
+++ image_renderer/dump_images.py
@@ -1,30 +1,31 @@
 import glob, os, torch
 from depth_anything_3.api import DepthAnything3
 
-# device = torch.device("cuda")
 model = DepthAnything3.from_pretrained("depth-anything/DA3NESTED-GIANT-LARGE")
 model = model.to(device=torch.device("cuda"))
 
-dataset_path = "/home/amila/datasets/kitti360/images"
+dataset_name = "matrixcity"
+dataset_path = f"/home/amila/datasets/{dataset_name}/images"
+out_path = f"/home/amila/Depth-Anything-3/outputs/{dataset_name}"
 images = sorted(glob.glob(os.path.join(dataset_path, "*.png")))
 
 export_args = {
     "gs_video": {
         "vis_depth": None,
         "trj_mode": "original",
-        "dump_images_dir": "/home/amila/Depth-Anything-3/outputs/images"
+        "dump_images_dir": f"{out_path}/images"
     }
 }
 
 prediction = model.inference(
     image=images,
     infer_gs=True,
-    export_dir="/home/amila/Depth-Anything-3/outputs",
-    export_format="gs_video",
+    export_dir=out_path,
+    export_format="gs_video-gs_ply",
     process_res=448,
     export_kwargs=export_args,
-    # align_to_input_ext_scale=True,
 )
+
 # prediction.processed_images : [N, H, W, 3] uint8   array
 print(prediction.processed_images.shape)
 # prediction.depth            : [N, H, W]    float32 array
